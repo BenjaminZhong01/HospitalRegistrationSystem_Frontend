30f98a4e48f8b6a87ebc35aa1138fe57a1e8db1a
Benjamin Zhong <renjiebenjamin@gmail.com>
2022-07-07 16:37:57 +0930

    feat: export and import data dictionary [videos 62, day 6]

--- vue-admin-template-test/src/views/dict/list.vue
+++ vue-admin-template-test/src/views/dict/list.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="app-container">
+        <div class="el-toolbar">
+            <div class="el-toolbar-body" style="justify-content: flex-start;">
+                <el-button type="text" @click="exportData"><i class="fa fa-plus"/> Export</el-button>
+                <el-button type="text" @click="importData"><i class="fa fa-plus"/> Import</el-button>
+            </div>
+        </div>
+
         <el-table
             :data="list"
             style="width: 100%"
@@ -30,6 +37,23 @@
                 </template>
             </el-table-column>
         </el-table>
+        <el-dialog title="Import" :visible.sync="dialogImportVisible" width="480px">
+            <el-form label-position="right"label-width="170px">
+                <el-form-item label="File">
+                    <el-upload
+                        :multiple="false"
+                        :on-success="onUploadSuccess"
+                        :action="'http://localhost:8202/admin/cmn/dict/importData'"
+                        class="upload-demo">
+                        <el-button size="small"type="primary">Upload</el-button>
+                        <div slot="tip" class="el-upload__tip">xls file only，no larger than 500kb</div>
+                    </el-upload>
+                </el-form-item>
+            </el-form>
+            <div slot="footer"class="dialog-footer">
+                <el-button @click="dialogImportVisible = false">cancel</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -37,13 +61,26 @@ import dict from '@/api/dict'
 export default {
     data() {
         return {
-            list:[] //data dictionary list
+            list:[], //data dictionary list
+            dialogImportVisible: false
         }
     },
     created() {
         this.getDictList(1)
     },
     methods: {
+        importData() {
+            this.dialogImportVisible = true
+        },
+        onUploadSuccess() {
+            //close window
+            this.dialogImportVisible = false
+            //refresh page
+            this.getDictList(1)
+        },
+        exportData() {
+            window.location.href = "http://localhost:8202/admin/cmn/dict/exportData"
+        },
         getDictList(id) {
             dict.dictList(id)
                 .then(response => {
